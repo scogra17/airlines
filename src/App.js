1af10b2b7163bb7bf8dde2a8e689d0ec10e5c45e
Scott Graham <scttgrhm7+public@gmail.com>
2022-04-19 15:25:04 -0700

    Display raw data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data'
 
 const App = () => (
   <div className="app">
   <header className="header">
-    <h1 className="title">Airline Routes!</h1>
+    <h1 className="title">Airline Routes</h1>
   </header>
   <section>
     <p>
       Welcome to the app!
     </p>
   </section>
+  <ul>
+    {data.routes.map(route => <li>{route.airline} | {route.src} | {route.dest}</li>)}
+  </ul>
 </div>
 )
 
