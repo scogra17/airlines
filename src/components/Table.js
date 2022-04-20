5fc27f6f181decebca61134a2e2358c26eb5b2b6
Scott Graham <scttgrhm7+public@gmail.com>
2022-04-20 14:52:39 -0700

    Add table pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Table = ({ className, columns, rows, format }) => {
+const rowsToDispay = (values, startIdx, count) => {
+  return values.slice(startIdx, startIdx+count)
+}
+
+const Table = ({ className, columns, rows, format, perPage=25 }) => {
+  const [startIdx, setStartIdx] = useState(0)
+
+  const handleLoadNextPage = () => {
+    if ((startIdx + perPage) < rows.length) {
+      setStartIdx(startIdx + perPage)
+    }
+  }
+  
+  const handleLoadPreviousPage = () => {
+    if ((startIdx - perPage) >= 0) {
+      setStartIdx(startIdx - perPage)
+    }
+  }
+
   return (
-    <table className={className}>
-      <thead>
-        <tr>
-          {columns.map(heading => <th>{heading.name}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {rows.map(route => {
-        return (
-            <tr>
-              <td>{format("airline", route.airline)}</td>
-              <td>{format("src", route.src)}</td> 
-              <td>{format("dest", route.dest)}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    <div>
+      <p>Showing {startIdx} - {startIdx + perPage} of {rows.length} total routes</p>
+      <button onClick={handleLoadPreviousPage}>Previous Page</button>
+      <button onClick={handleLoadNextPage}>Next Page</button>
+      <table className={className}>
+        <thead>
+          <tr>
+            {columns.map(heading => <th>{heading.name}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {rowsToDispay(rows, startIdx, perPage).map(route => {
+          return (
+              <tr>
+                <td>{format("airline", route.airline)}</td>
+                <td>{format("src", route.src)}</td> 
+                <td>{format("dest", route.dest)}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
